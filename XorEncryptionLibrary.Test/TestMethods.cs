1eb4b2612c5a0588082143a0668db8e2efb97522
William Mortl <wmortl@Locutus.local>
2015-07-14 13:00:08 -0500

    skeleton checkin

---
+++ XorEncryptionLibrary.Test/TestMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XorEncryptionLibrary.Test
+{
+    [TestClass]
+    public class TestMethods
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
